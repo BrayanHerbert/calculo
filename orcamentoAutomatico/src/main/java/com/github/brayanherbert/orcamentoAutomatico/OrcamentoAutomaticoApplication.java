753b75f1eae200a59615b8868483d7e97a7c768f
Brayan Herbert <eubrayan34@gmail.com>
2025-04-18 17:19:48 -0300

    Primeiro Commit

---
+++ orcamentoAutomatico/src/main/java/com/github/brayanherbert/orcamentoAutomatico/OrcamentoAutomaticoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brayanherbert.orcamentoAutomatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoAutomaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoAutomaticoApplication.class, args);
+	}
+
+}
---
+++ orcamentoAutomatico/src/test/java/com/github/brayanherbert/orcamentoAutomatico/OrcamentoAutomaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brayanherbert.orcamentoAutomatico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoAutomaticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
